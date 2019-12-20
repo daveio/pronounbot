df659677a058d528fbeb3c830ff9a2971b298a88
Dave Williams <dave@dave.io>
2019-12-20 23:25:16 +0000

    ready to rock

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Http = require('http');
+const httpPort = 3838;
 
 const Discord = require('discord.js');
 
@@ -13,26 +14,34 @@ validate.token(token);
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('I\'m up!');
+  console.log("I'm up!");
 });
 
 client.on('message', message => {
-  const pronouns = pronounify.extractPronouns(message.content)
-
-  if (pronouns) {
-    const guildRoles = message.guild.roles;
-    const member = message.member;
-    const returnMessage = pronounify.setPronouns(pronouns, guildRoles, member);
-    if (returnMessage) {
-      message.channel.send(returnMessage);
-    } else {
-      console.log('there was a problem setting roles, missing role?');
+  let checkForCommands = false;
+  if (message.guild === null && message.author.username != 'pronounbot') {
+    message.reply(`Please say commands in a channel, so the bot knows which instance you're from.
+    You can say the following:
+    \`list available pronouns   \` -- Get a list of available pronoun sets
+    \`my pronouns are IDENTIFIER\` -- Set your pronouns to IDENTIFIER`);
+  } else if (message.author.username != 'pronounbot') {
+    const pronouns = pronounify.extractPronouns(message);
+    pronounify.listPronouns(message);
+    pronounify.addPronouns(message);
+    if (pronouns) {
+      const guildRoles = message.guild.roles;
+      const member = message.member;
+      const returnMessage = pronounify.setPronouns(pronouns, guildRoles, member);
+      if (returnMessage) {
+        message.channel.send(returnMessage);
+      } else {
+        console.log('there was a problem setting roles, missing role?');
+      }
     }
   }
-
 });
 
-client.on('error', (err) => {
+client.on('error', err => {
   console.log(err);
 });
 
@@ -40,7 +49,6 @@ client.login(token);
 
 // For setting up uptime robot / monitoring
 const monitoringServer = Http.createServer((request, response) => {
-
   const statuses = {
     0: 'Ready',
     1: 'Connecting',
@@ -55,9 +63,9 @@ const monitoringServer = Http.createServer((request, response) => {
   response.end();
 });
 
-monitoringServer.listen(3838, (err) => {
+monitoringServer.listen(httpPort, err => {
   if (err) {
     return console.log('monitoring crashed', err);
   }
-  console.log(`monitoring server is listening on ${3838}`);
+  console.log(`monitoring server is listening on ${httpPort}`);
 });
--- src/pronouns.js
+++ src/pronouns.js
@@ -1,27 +1,68 @@
 const pronounsList = require('./pronounsList');
+const pronounsArray = Object.keys(pronounsList);
 
-const extractPronouns = function (command) {
+const extractPronouns = function(message) {
+  let command = message.content;
   let splitCommand;
   if (command.toLowerCase().startsWith('my pronouns are')) {
     try {
       splitCommand = command.split('are')[1].trim();
     } catch (e) {
-      console.log('there was a problem splitting the command')
+      console.log('there was a problem splitting the command');
     }
   }
   return pronounsList[splitCommand];
-}
+};
 
-const setPronouns = function (roleName, guildRoles, member) {
+const listPronouns = message => {
+  let command = message.content;
+  if (command.toLowerCase().startsWith('list available pronouns')) {
+    let respText = '';
+    pronounsArray.forEach(element => {
+      respText += `- \`${element}\`\n`;
+    });
+    message.reply('the list of available pronouns has been sent via private message.');
+    message.author.send(
+      'Set your pronouns by saying `my pronouns are IDENTIFIER`, where IDENTIFIER is one of the following:\n' + respText
+    );
+  }
+};
+
+const setPronouns = function(roleName, guildRoles, member) {
   const role = guildRoles.find(r => r.name === roleName);
   if (role) {
-    member.addRole(role).catch(console.error);
-    return `Success! We've set your pronouns to ${roleName}`;
+    member
+      .removeRoles(
+        pronounsArray.map(rn => {
+          return guildRoles.find(i => i.name === rn);
+        })
+      )
+      .then(ret => {
+        member.addRole(role).catch(console.error);
+      });
+    return `Success! We've set your pronouns to ${roleName}. Click on your avatar in the member list to see them.`;
   }
   return null;
-}
+};
+
+const addPronouns = message => {
+  let command = message.content;
+  if (command.toLowerCase().startsWith('do pronoun setup') && process.env.ALLOW_PRONOUN_SETUP === 'yes') {
+    let guild = message.guild;
+    pronounsArray.forEach(r => {
+      guild
+        .createRole({
+          name: r
+        })
+        .then(role => console.log(`Created new role with name ${role.name}`))
+        .catch(console.error);
+    });
+  }
+};
 
 module.exports = {
   extractPronouns,
-  setPronouns
-}
+  setPronouns,
+  listPronouns,
+  addPronouns
+};
